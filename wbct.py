a2559f3eb296dc56bc97d1815482913c2f4d861d
Pierre Pasquet <pierre.pasquet@bluecargo.io>
2022-06-24 10:59:51 -0400

    WBCT results

--- wbct.py
+++ wbct.py
@@ -1,19 +1,23 @@
 import requests
 from dotenv import dotenv_values
 import json
+from bs4 import BeautifulSoup
+import tqdm
+import pandas as pd
+import random
 from html.parser import HTMLParser
 
 config = dotenv_values(".env")
 
 HEADERS = {
     "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/97.0.4692.71 Safari/537.36",
-    "Content-Type": "application/x-www-form-urlencoded"
+    "Content-Type": "application/x-www-form-urlencoded",
+    "Connection": "keep-alive",
+    "Accept-Encoding": "gzip, deflate, br",
+    "Accept": "*/*"
 }
 
-SCAC_PER_TERMINAL = {
-    "tti": ['CMLF', 'JCTK', 'PARB', 'KFKC', 'ADXH', 'SGAT', 'ROLY', 'TIKG', 'MHBQ', 'KTEV', 'PSED', 'TGSM', 'JDLC', 'YMLU', 'MXDP', 'AQEG', 'HKTH', 'BOLS', 'PACL', 'SNEA', 'OSGB', 'EGHC', 'DEUC', 'DWCH', 'AANA', 'USON', 'CFMM', 'NOTQ', 'NGLN', 'ISNC', 'RLUP', 'CCOO', 'CVDG', 'TRUO', 'WLGO', 'DPHE', 'CPTD', 'TCED', 'EOIA', 'RTIB', 'CGLQ', 'KCNT', 'ELLC', 'FASR', 'JDXQ', 'MAHD', 'TWWL', 'WHSK', 'EFAL', 'RYJG', 'MWCJ', 'MRYV', 'PILE', 'WTTE', 'ATKF', 'OTDW', 'PPCW', 'GOHE', 'HPRN', 'GGEJ', 'TLDV', 'LSFT', 'LFIE', 'GPML', 'KNIG', 'ADXF', 'MKMG', 'MXLH', 'GSXP', 'BLHT', 'PTID', 'FLCB', 'ADDP', 'DHXN', 'GBKV', 'RRBW', 'CUOT', 'KYRT', 'PCAK', 'CWTP', 'BMXQ', 'DYKI', 'AMPF', 'PRMI', 'CURA', 'LMCJ', 'LRGR', 'BGIJ', 'FNNK', "'/'", 'USSN', 'OSHB', 'JTFJ', 'SWHH', 'TMOD', 'OFOP', 'DGNN', 'FATR', 'GPON', 'MAEU', 'FRQT', 'GSDY', 'VENT', 'TKPS', 'XPOK', 'FMAE', 'OERT', 'WDWI', 'DNSL', 'VPEC', 'TIYI', 'ETSE', 'CJXE', 'FTJO', 'MCLQ', 'GJGQ', 'PCFA', 'CPNH', 'ROJD', 'THSL', 'SBYB', 'FHRC', 'BULK', 'EVFQ', 'VUTP', 'PISN', 'RKGC', 'LBTI', 'DLMT', 'COCA', 'RCTQ', 'LVES', 'IMPC', 'EFTT', 'ZHTP', 'FCKI', 'SELK', 'ECRR', 'SNLP', 'GCTN', 'JCDP', 'HDDR', 'TFCH', 'WTIK', 'PCFE', 'NWDW', 'PAFA', 'GATI'],
-    "wbct": ['IDLR', 'CMLF', 'JCTK', 'PARB', 'TSON', 'ADXH', 'SGAT', 'MHBQ', 'KTEV', 'PSED', 'JDLC', 'MXDP', 'AQEG', 'HKTH', 'EGHC', 'DEUC', 'ULSB', 'USON', 'CVDG', 'RLUP', 'WLGO', 'TRUO', 'TCED', 'OVFR', 'CGLQ', 'ELLC', 'ODYL', 'UNIC', 'ITIK', 'WHSK', 'PRQL', 'MWCJ', 'OTDW', 'GGEJ', 'LSFT', 'KNIG', 'ADXF', 'GSXP', 'BLHT', 'PTID', 'DULL', 'RRBW', 'GCIS', 'SONW', 'AMZN', 'PCAK', 'CWTP', 'NART', 'HDMU', 'PRMI', 'AMPF', 'CURA', 'GCNF', 'QXLC', 'USSN', 'OSHB', 'TMOD', 'OFOP', 'DGNN', 'GPON', 'FRQT', 'GSDY', 'VENT', 'XPOK', 'FDIH', 'OERT', 'WDWI', 'TIYI', 'ETSE', 'CJXE', 'PNDH', 'FTJO', 'CHRW', 'THSL', 'TILG', 'BULK', 'DLMT', 'COCA', 'MZSS', 'RCTQ', 'EFTT', 'ZHTP', 'FCKI', 'SELK', 'ECRR', 'TMYK', 'GCTN', 'WTIK', 'NWDW', 'GATI']
-}
+SCACS = ['IDLR', 'CMLF', 'JCTK', 'PARB', 'TSON', 'ADXH', 'SGAT', 'MHBQ', 'KTEV', 'PSED', 'JDLC', 'MXDP', 'AQEG', 'HKTH', 'EGHC', 'DEUC', 'ULSB', 'USON', 'CVDG', 'RLUP', 'WLGO', 'TRUO', 'TCED', 'OVFR', 'CGLQ', 'ELLC', 'ODYL', 'UNIC', 'ITIK', 'WHSK', 'PRQL', 'MWCJ', 'OTDW', 'GGEJ', 'LSFT', 'KNIG', 'ADXF', 'GSXP', 'BLHT', 'PTID', 'DULL', 'RRBW', 'GCIS', 'SONW', 'AMZN', 'PCAK', 'CWTP', 'NART', 'HDMU', 'PRMI', 'AMPF', 'CURA', 'GCNF', 'QXLC', 'USSN', 'OSHB', 'TMOD', 'OFOP', 'DGNN', 'GPON', 'FRQT', 'GSDY', 'VENT', 'XPOK', 'FDIH', 'OERT', 'WDWI', 'TIYI', 'ETSE', 'CJXE', 'PNDH', 'FTJO', 'CHRW', 'THSL', 'TILG', 'BULK', 'DLMT', 'COCA', 'MZSS', 'RCTQ', 'EFTT', 'ZHTP', 'FCKI', 'SELK', 'ECRR', 'TMYK', 'GCTN', 'WTIK', 'NWDW', 'GATI']
 
 def get_credential(scac):
     url = f"{config['CORE_API_URL']}/appointments/parser/wbct/authentications"
@@ -39,8 +43,9 @@ def get_consignees(scac, credentials):
         #     0: "true",
         #     1: "false"
         # },
+        "ShowAllImportContainersStillOut": "true",
         "CntrSscoCode": "",
-        "LastExitGateDays": "21",
+        "LastExitGateDays": "2000",
         # "ShowAllActivePendingAppts": {
         #     0: "true",
         #     1: "false"
@@ -48,7 +53,7 @@ def get_consignees(scac, credentials):
         "ShowAllActivePendingAppts": "false",
         # "ApptSscoCode": "",
         "ContainerNumber": "",
-        "SkipCheckDigitRecalculation": "False"
+        "SkipCheckDigitRecalculation": "false"
     }
     proxies = {
         "http": credential.get("metadata", {}).get("proxy"),
@@ -59,21 +64,43 @@ def get_consignees(scac, credentials):
         "Cookie": credential.get("headers", {}).get("Cookie")
     }
     response = requests.post(
-        "https://voyagertrack.portsamerica.com/SearchEmptyIns",
+        "https://voyagertrack.portsamerica.com/Report/EmptyIn/SearchEmptyIns?pageSize=2000",
         data=data,
         proxies=proxies,
         headers=headers,
         verify=False
     )
-    # response = requests.get(
-    #     "https://voyagertrack.portsamerica.com/Report/EmptyIn/EmptyIn?_=1656002791739",
-    #     data=data,
-    #     proxies=proxies,
-    #     headers=headers,
-    #     verify=False
-    # )
-    print(response.text)
+    html = BeautifulSoup(response.text, "html.parser")
+    # tab = html.find("table", {"class": "appointment collapsible"})
+    tab = html.find("table", {"class": "appointment collapsible"})
+    rows = tab.find_all("tbody")[0].find_all("tr", {"data-move-type": "EmptyIn"})
+    consignees = [
+        row.find_all("td")[8].text.strip()
+        for row in rows
+    ]
+    return set([x for x in consignees if x != ""])
 
 if __name__ == "__main__":
-    cred = get_credential("ADXH")
-    get_consignees("ADXH", cred)
+    data = []
+    results = {}
+    # pool = random.sample(SCACS, 5)
+    for scac in tqdm.tqdm(SCACS):
+        try:
+            cred = get_credential(scac)
+            consignees = get_consignees(scac, cred)
+            for consignee in consignees:
+                data.append([scac, consignee])
+            results[scac] = True
+        except:
+            results[scac] = False
+    print(f"Total: {len(results)}\tFailed: {len([x for x in results if not x])}\tSuccess: {len([x for x in results if x])}")
+    df = pd.DataFrame(data=data, columns=["SCAC", "consignee"])
+    df.to_csv("wbct.csv")
+
+
+
+    # print(config)
+
+    # print("\n")
+    # print(cred)
+    # print(json.dumps(cred["adpexpress"].get("headers").get("Cookie"), indent=4)[1:-1])
